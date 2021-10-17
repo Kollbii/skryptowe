6ea45c4567b161af043ba1e4c43a55892b31bbc4
Kollbii <lkollbek@gmail.com>
2021-10-17 22:33:03 +0200

    Lab1 with tests

--- Lab1/subs.py
+++ Lab1/subs.py
@@ -1,3 +1,6 @@
+import sys, os
+import signal
+
 class Channel:
     def __init__(self, name, subs_count, description):
         self.name = name
@@ -12,9 +15,6 @@ class Channel:
         self.description = ''
         return True
 
-    def get_description(self):
-        return self.description
-
 class Account:
     def __init__(self, name):
         self.subs_channels = []
@@ -23,6 +23,7 @@ class Account:
     def subscribe(self, channel):
         if channel not in self.subs_channels:
             self.subs_channels.append(channel)
+            channel.subs_count += 1
             return True
         else:
             print("{} is already subscribed to {}".format(self.name, channel.name))
@@ -31,6 +32,7 @@ class Account:
     def unsubscribe(self, channel):
         if channel in self.subs_channels:
             self.subs_channels.remove(channel)
+            channel.subs_count -= 1
             return True
         else:
             print("{} is not subscribed to {}".format(self.name, channel.name))
@@ -41,12 +43,19 @@ def action(acc, option, channel):
     if option == 1:
         if acc.subscribe(channel):
             print("{} subscribed to {}".format(acc.name, channel.name))
+            print("Current subscribers to {}: {}".format(channel.name, channel.subs_count))
+
+
     elif option == 2:
         if acc.unsubscribe(channel):
-            print("{} unsubscribed to {}".format(acc.name, channel.name))  
+            print("{} unsubscribed to {}".format(acc.name, channel.name))
+            print("Current subscribers to {}: {}".format(channel.name, channel.subs_count))
 
+def handle_exit(sigint, frame):
+    exit(0)
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, handle_exit)
     accounts = {
         1: Account("User 1"),
         2: Account("User 2")
@@ -71,16 +80,18 @@ if __name__ == '__main__':
     # 3 - add description
     # 4 - remove description
     # 5 - info 
-    # 6 - add channel
+    # 6 - add user
+    # 7 - add channel
     opt = 1
     print("Option: ")
     opt = int(input())
     while opt != 0:
+        os.system('clear')
         try:
-            if opt not in [3, 4, 5, 6]:
+            if opt in [1, 2]:
                 print("Account: ")
                 acc = accounts[int(input())]
-                print("Channel number 1-4: ")
+                print("Channel number 1-{}: ".format(len(channels)))
                 chn = channels[int(input())]
                 action(acc, opt, chn)
             elif opt == 3:
@@ -97,15 +108,16 @@ if __name__ == '__main__':
             elif opt == 5:
                 for num in accounts:
                     for chn in accounts[num].subs_channels:
-                        print("{} is subscribed to {} -> Description: {}".format(accounts[num].name, chn.name, chn.description))
+                        print("{} is subscribed to {};\tSub Count: {};\tDescription: {}".format(accounts[num].name, chn.name, chn.subs_count,chn.description))
             elif opt == 6:
                 num = len(accounts)
                 new_name = str(input("New user name: "))
                 accounts[num + 1] = Account(new_name)
-            
+            elif opt == 7:
+                num = len(channels)
+                new_name = str(input("New channel name: "))
+                channels[num + 1] = Channel(new_name, 0, "")
             print("Option: ")
             opt = int(input())
         except KeyboardInterrupt:
-            quit()
-    else:
-        quit()
+            sys.exit(0)
