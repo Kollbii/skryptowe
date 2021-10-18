71e2963d93ac3d4423af18fd185d8407402946c8
Kollbi <lkollbek@gmail.com>
2021-10-18 22:47:10 +0200

    Lab2 fixes. Tests.

--- Lab2/skrypt.py
+++ Lab2/skrypt.py
@@ -1,22 +1,23 @@
-import re
+import re, sys
 
 def extract(text: str):
-    for num in re.findall('[-]?[0-9]+', text): # [0-9] - All single digits [0-9]+ - sequence of single digits
+    for num in re.findall('[-]?[0-9]+', text):  # [0-9]+ - All single digits. 
         yield int(num)
-    for string in re.findall('[^-\s+0-9]\D+', text): # \D -> everything but not number. + -> sequence. Optional [^0-9]+. [^\s-] Not white char and dash.
-        yield str(string).strip()  # Optional white chars removal
+    # for string in re.findall('[^-\s+0-9][a-zżźćńąśłęóA-ZŻŹĆŃĄŚŁĘÓ]+', text):
+    for string in re.findall('[^\W|^\d|^\s+]+', text):  # Testing from left to right '|'. \W == [^a-zA-Z0-9_]. \d - any digits. \s matches white space like [ \t\n\r\f\v]
+        yield str(string).strip()
 
 def start():
     while True:
         try:
-            text:str = input()
-            for element in extract(text):
+            for element in extract(str(sys.stdin.readline())):
                 if type(element) == int:
                     print("\tLiczba: {}".format(element))
                 elif type(element) == str:
                     print("\tWyraz: {}".format(element))
-        except:
-            print("Error: Could not detect proper input. Exit CTRL + Z")
+        except KeyboardInterrupt:
+            print("Error/Exit msg")
+            break
 
 if __name__ == "__main__":
     start()
--- Lab2/test.py
+++ Lab2/test.py
@@ -1,21 +1,22 @@
-from typing import AnyStr
 import skrypt
 import unittest
 
 class Test_TestSkrypt(unittest.TestCase):
-    
     def test_word_numer(self):
         for answer in skrypt.extract("polskiText123more"):
             self.assertIn(answer, ["polskiText", 123, "more"])
     
     def test_only_word(self):
-        for answer in skrypt.extract("dlugiteksttylkoze znakami i   spacjami"):
-            self.assertEqual(answer, "dlugiteksttylkoze znakami i   spacjami")
+        for answer in skrypt.extract("DlugiTekstTylko znakami i   spacjami"):
+            self.assertIn(answer, ["DlugiTekstTylko", "znakami", "i",   "spacjami"])
 
     def test_only_nums(self):
         for answer in skrypt.extract("123 1355 -4359 321 0"):
             self.assertIn(answer, [123, 1355, -4359, 321, 0])
 
+    def test_combine_all(self):
+        for answer in skrypt.extract("Full dis-99closure, Morty - it's not. Temporary superintell11igence."):
+            self.assertIn(answer, [11,-99, "Full", "dis", "closure", "Morty", "it", "s", "not", "Temporary", "superintell", "igence"])
 
 if __name__ == '__main__':
     unittest.main()
