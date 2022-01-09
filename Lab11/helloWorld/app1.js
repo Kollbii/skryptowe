e6bf76b9de484b46a59822a4ed13c8adeb939721
Kollbi <lkollbek@gmail.com>
2022-01-09 22:27:47 +0100

    half/full

--- Lab11/helloWorld/app1.js
+++ Lab11/helloWorld/app1.js
@@ -1,9 +1,13 @@
 // No use of any template system
 var express = require('express'),
     logger = require('morgan');
+const path = require('path');
+var fs = require('fs')
 var app = express();
 var x = 1;
-var y = 2;
+var y = 9;
+
+function sum(x, y){return x + y};
 
 // Determining the contents of the middleware stack
 app.use(logger('dev'));                         // Place an HTTP request recorder on the stack — each request will be logged in the console in 'dev' format
@@ -11,10 +15,37 @@ app.use(logger('dev'));                         // Place an HTTP request recorde
 
 // Route definitions
 app.get('/', function (req, res) {     // The first route
-    res.send('<h1>Hello World!</h1>'); // Send a response to the browser
+    res.send(`<h1>Summing numbers hardcoded inside!</h1><p> ${x} + ${y} = ${sum(x, y)}</p>`);
 });
 
+app.get('/json/:name', (req, res) => {
+    let data = JSON.parse(fs.readFileSync(req.params.name + '.json'));
+    
+    repr = '';
+    repr += `
+    <table>
+    <tr>
+        <th>x</th>
+        <th>Operation</th>
+        <th>y</th>
+        <th>Result</th>
+    </tr>
+    `
+    data.forEach(element => {
+        repr += '<tr>'
+        repr += `<td>${element.x}</td>`;
+        repr += `<td>${element.o}</td>`;
+        repr += `<td>${element.y}</td>`;
+        result = eval(element.x + element.o + element.y); // Make operation happen
+        repr += `<td>${result}</td>`;
+        repr += "</tr>";
+    });
+    repr += `</table>`
+    res.send(`<h1>${repr}</h1>`);
+});
 // The application is to listen on port number 3000
 app.listen(3000, function () {
     console.log('The application is available on port 3000');
 });
+
+// To run: npm run app1
--- Lab11/helloWorld/app2.js
+++ Lab11/helloWorld/app2.js
@@ -1,12 +1,15 @@
 // Application using the 'Pug' template system
 var express = require('express'),
     logger = require('morgan');
+var fs = require('fs');
 var app = express();
 var x = 1;
-var y = 2;
+var y = 5;
+
+function sum(x, y){return x + y};
 
 // Configuring the application
-app.set('views', __dirname + '/views'); // Files with views can be found in the 'views' directory
+app.set('views', __dirname + '\\views\\'); // Files with views can be found in the 'views' directory
 app.set('view engine', 'pug');          // Use the 'Pug' template system
 
 // Determining the contents of the middleware stack
@@ -15,11 +18,37 @@ app.use(logger('dev'));                         // Add an HTTP request recorder 
 
 // Route definitions
 app.get('/', function (req, res) {      // The first route
-    // res.render('index', {pretty:true}); // Render the 'index' view in 'pretty' mode — the resulting HTML code will be indented — the 'pretty' option has the 'deprecated' status — in the future it will not be supported
-    res.render('index '); // Render the 'index' view; because the 'pretty' mode is, by default, turned off so the resulting HTML will be without indentation
+    res.send(`<h1>Summing numbers hardcoded inside!</h1><p> ${x} + ${y} = ${sum(x, y)}</p>`);
 });
 
+app.get('/json/:name', (req, res) => {
+    let data = JSON.parse(fs.readFileSync(req.params.name + '.json'));
+    
+    repr = '';
+    repr += `
+    <table class='table'>
+    <tr>
+        <th>x</th>
+        <th>Operation</th>
+        <th>y</th>
+        <th>Result</th>
+    </tr>
+    `
+    data.forEach(element => {
+        repr += '<tr>'
+        repr += `<td>${element.x}</td>`;
+        repr += `<td>${element.o}</td>`;
+        repr += `<td>${element.y}</td>`;
+        result = eval(element.x + element.o + element.y); // Make operation happen
+        repr += `<td>${result}</td>`;
+        repr += "</tr>";
+    });
+    repr += `</table>`
+    res.send(`<h1>${repr}</h1>`);
+});
 // The application is to listen on port number 3000
 app.listen(3000, function () {
     console.log('The application is available on port 3000');
 });
+
+// To run: npm run app2
